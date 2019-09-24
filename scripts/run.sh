a48bef7e0a12411ac93ddbe13e11052841059208
yoandryhc85 <51792640+yoandryhc85@users.noreply.github.com>
2019-09-24 16:49:13 -0300

    changes
    changes

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,6 +3,7 @@
 ECR_URL="980422662115.dkr.ecr.us-west-1.amazonaws.com/web-instride:latest" 
 Container_Name="WebInstrideContainer"
  
-echo "Run container" sudo docker run -dit -p 85:80 --name $Container_Name $ECR_URL
+echo "Run container" 
+sudo docker run -dit -p 85:80 --name $Container_Name $ECR_URL
 
 
