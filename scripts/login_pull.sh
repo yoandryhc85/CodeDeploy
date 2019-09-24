b256711792ae031d4ad09efeecb79af1ac9b9199
yoandryhc85 <51792640+yoandryhc85@users.noreply.github.com>
2019-09-24 15:22:35 -0300

    change
    change

--- scripts/login_pull.sh
+++ scripts/login_pull.sh
@@ -6,4 +6,6 @@ Container_Name="WebInstrideContainer"
 echo "login to ECR"
 aws ecr get-login --region us-west-1 --no-include-email | bash
 
+echo "Pull imagen en el equipo"
+
 sudo docker pull 980422662115.dkr.ecr.us-west-1.amazonaws.com/web-instride:latest 
