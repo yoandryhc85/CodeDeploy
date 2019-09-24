d3ee7d556aeab60d6aed650046950721ff7d095b
yoandryhc85 <51792640+yoandryhc85@users.noreply.github.com>
2019-09-24 14:59:11 -0300

    Update login_pull.sh

--- scripts/login_pull.sh
+++ scripts/login_pull.sh
@@ -4,7 +4,6 @@ ECR_URL="980422662115.dkr.ecr.us-west-1.amazonaws.com/web-instride:latest"
 Container_Name="WebInstrideContainer"
  
 echo "login to ECR"
-### aws ecr get-login --region us-west-1 --no-include-email | bash
-### echo "pulling the images"
+aws ecr get-login --region us-west-1 --no-include-email | bash
 
 sudo docker pull 980422662115.dkr.ecr.us-west-1.amazonaws.com/web-instride:latest 
